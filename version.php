9836c644ae833d89bb81103edf8ce74eb7748499
valery <valery.fremaux@gmail.com>
2016-11-18 16:03:03 +0100

    Automated sync from SVN

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2016060600;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2015111100;        // Requires this Moodle version.
+$plugin->requires  = 2016051900;        // Requires this Moodle version.
 $plugin->component = 'block_publishflow'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '3.0.0 (Build 2016060600)';
+$plugin->release = '3.1.0 (Build 2014031900)';
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.0.0000';
+$plugin->codeincrement = '3.1.0000';
