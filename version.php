69104c35cefc590f8639bf783d24639b4defd401
valery <valery.fremaux@gmail.com>
2018-03-24 20:17:45 +0100

    resync al versions

--- version.php
+++ version.php
@@ -26,11 +26,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016060600;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2016120500;        // Requires this Moodle version.
+$plugin->version   = 2018031801;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2017050500;        // Requires this Moodle version.
 $plugin->component = 'block_publishflow'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '3.2.0 (Build 2014031900)';
+$plugin->release = '3.3.0 (Build 2018031801)';
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.2.0000';
+$plugin->codeincrement = '3.3.0002';
